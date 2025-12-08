06b108a3d429c8a66d79613d46e3dbaa984b26a3
vnieutin-dev <vnieutin.dev@gmail.com>
2025-12-08 13:38:08 +0100

    basic header and footer

---
+++ assets/js/include.js
@@ -0,0 +1,9 @@
+async function loadPartial(id, file) {
+    const el = document.getElementById(id);
+    const response = await fetch(file);
+    const html = await response.text();
+    el.innerHTML = html;
+}
+
+loadPartial("header", "/partials/header.html");
+loadPartial("footer", "/partials/footer.html");
